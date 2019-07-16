63160791c4269e91bca4e0e7934d9060418e4845
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-16 12:01:11 +0900

    Blog 객체 html에 구현

--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    blog = Blog.objects
+    return render(request, 'index.html', {'blog' : blog})
