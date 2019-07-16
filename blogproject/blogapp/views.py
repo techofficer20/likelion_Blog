6f43001c53043428d13fce3fd7b2dcca2e020672
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-16 13:06:52 +0900

    paginator

--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Blog
 # Create your views here.
 
 def index(request):
-    blog = Blog.objects
-    return render(request, 'index.html', {'blog' : blog})
+    #블로그 모든 글들을 대상으로
+    blog_list=Blog.objects.all()
+    #블로그 객체 세 개를 한 페이지로 자르기
+    paginator = Paginator(blog_list,3)
+    #request된 페이지가 뭔지를 알아내고 ( request페이지를 변수에 담아냄 )
+    page = request.GET.get('page')
+    #request된 페이지를 얻어온 뒤 return 해 준다
+    posts = paginator.get_page(page)
+    return render(request, 'index.html', {'posts' : posts})
 
 def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk = blog_id)
