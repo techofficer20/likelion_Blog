8249546d80ee6a739e4246feaefae1f39c2a70a3
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-20 17:07:30 +0900

    초기화

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\techofficer20\desktop\web\likelion\likelion_blog\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
